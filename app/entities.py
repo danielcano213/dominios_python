709edc4a063e348e72986251f2474a2fbc956dca
Aprendiz <Aprendiz@BOGDFPCGMF4149.FormacionSena.RED>
2024-02-13 16:48:27 -0500

    requirements.txt

--- app/entities.py
+++ app/entities.py
@@ -18,4 +18,28 @@ class Cliente():
     def listCar(self):
         for i in self.lista_carros:
             print("Carro con placas: " + i.placa )
-  
+
+class Cupo():
+
+    def __init__(self, letra):
+        self.letra = letra 
+
+class Pago():
+
+    def __init__(self,
+                 fecha_inicio,
+                 hora_inicio,
+                 fecha_fin,
+                 hora_fin,
+                 valor,
+                 carro,
+                 cupo,
+                 empleado):
+       self.fecha_inicio = fecha_inicio
+       self.hora_inicio = hora_inicio
+       self.fecha_fin = fecha_fin 
+       self.hora_fin = hora_fin
+       self.valor = valor
+       self.carro = carro
+       self.cupo = cupo
+       self.empleado = empleado
